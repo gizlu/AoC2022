19d44ae214e32f1cc2e579303b319379874cf464
gizlu <gizlu@mailbox.org>
2022-12-02 23:22:42 +0100

    Reorganize day1 solutions
    Replace separate script files for sake of one markdown file

--- day1/task1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-awk -v RS="\n\n" -F "\n" '{sum=0; for (i=1; i<=NF; ++i) sum += $i; print sum}' $1 | sort | tail -n1
--- day1/task2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-awk -v RS="\n\n" -F "\n" '{sum=0; for (i=1; i<=NF; ++i) sum += $i; print sum}' $1 | sort | tail -n3 |
-    awk '{sum+=$1} END{print sum}'
